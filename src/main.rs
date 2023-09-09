6bd9975b2f4749f4d25813fc4e026619cf0f240e
daimbk <daimbkhalid@gmail.com>
2023-09-09 17:38:48 +0500

    feat: main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use pixel_art::converter::pixelate::{ load_image, save_image, pixelate_image };
+
 fn main() {
-    println!("Hello, world!");
+    // load image
+    let input_image = load_image("./images/new/img.jpg").expect("Failed to load image");
+
+    // convert image
+    let output_image = pixelate_image(&input_image, 4);
+
+    // save image
+    save_image(&output_image, "./images/pixelated/img.jpg").expect("Failed to save pixel art");
 }
